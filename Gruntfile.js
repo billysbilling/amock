34ad684568bda0e038fc1324c16f0d8586d1d0bc
Sebastian Seilund <sebastian@seilund.com>
2013-11-11 17:20:10 -0800

    added initial files

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        'billy-builder': {
+            title: 'amock'
+        }
+    });
+
+    grunt.loadNpmTasks('billy-builder');
+};
